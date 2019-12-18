a149445c6529e0d9e4957725c92693f6ba06225e
Mahmoud Khalafallah <mahmoudkhalafallah92@gmail.com>
2019-12-19 00:14:38 +0200

    add jest

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,8 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+import React from 'react'
+import { render } from '@testing-library/react'
+import App from './App'
+import 'jest-fetch-mock'
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+test('App renders', () => {
+    render(<App />)
+})
