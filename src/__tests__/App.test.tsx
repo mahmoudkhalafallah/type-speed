7da22110e0dce46c194751df43f09cc86829e5c3
Mahmoud Khalafallah <mahmoudkhalafallah92@gmail.com>
2019-12-21 22:10:47 +0200

    fix failing app test

--- src/__tests__/App.test.tsx
+++ src/__tests__/App.test.tsx
@@ -1,7 +1,8 @@
 import React from 'react'
 import { render } from '@testing-library/react'
-import App from './App'
+import App from '../App'
 import 'jest-fetch-mock'
+import "mutationobserver-shim/MutationObserver"
 
 test('App renders', () => {
     render(<App />)
