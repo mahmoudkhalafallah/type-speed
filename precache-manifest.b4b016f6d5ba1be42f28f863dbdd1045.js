8f1af9287eccee828a2bca2212156a6fb958865f
Mahmoud Khalafallah <mahmoudkhalafallah92@gmail.com>
2019-12-21 17:42:56 +0200

    Updates

---
+++ precache-manifest.b4b016f6d5ba1be42f28f863dbdd1045.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c568522d0a7dc7caecf5da8558f38a3f",
+    "url": "/type-speed/index.html"
+  },
+  {
+    "revision": "b5a30473b02271772775",
+    "url": "/type-speed/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "e71fa877a3d5363147b3",
+    "url": "/type-speed/static/css/main.aedc4c42.chunk.css"
+  },
+  {
+    "revision": "b5a30473b02271772775",
+    "url": "/type-speed/static/js/2.76ed5b77.chunk.js"
+  },
+  {
+    "revision": "fd228f9b9a68ce6b23a6cf8b0a38a0e3",
+    "url": "/type-speed/static/js/2.76ed5b77.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e71fa877a3d5363147b3",
+    "url": "/type-speed/static/js/main.1cdb2fc6.chunk.js"
+  },
+  {
+    "revision": "289500145c7ac56372d5",
+    "url": "/type-speed/static/js/runtime-main.fa13eacd.js"
+  }
+]);
