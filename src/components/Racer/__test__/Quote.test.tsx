322d3fbff2902feea9cc8ee80a9af3599859bcd9
Mahmoud Khalafallah <mahmoudkhalafallah92@gmail.com>
2019-12-28 21:31:13 +0200

    update Quote test

--- src/components/Racer/__test__/Quote.test.tsx
+++ src/components/Racer/__test__/Quote.test.tsx
@@ -1,31 +1,29 @@
 import React from 'react'
-import Quote from '../Racer/Quote'
+import Quote from '../Quote'
 import { render } from '@testing-library/react'
 
 const str = 'Pork rump minim, laborum spare ribs pancetta lorem ut meatloaf ullamco swine sint voluptate pariatur. Ullamco mollit consequat est. Esse brisket spare ribs ipsum eiusmod t-bone mollit veniam. Exercitation short loin filet mignon, tenderloin tail anim kevin ribeye minim beef velit esse salami. Rump et do landjaeger.'.split(' ')
 
 test('Quote rendered correctly', () => {
-    const component = render(
+    const { baseElement } = render(
         <Quote data={str} index={0} length={0} isValid={false} />,
     )
 
-    expect(component).toMatchSnapshot()
+    expect(baseElement).toMatchSnapshot()
 })
 
 test('Quote is valid', () => {
-    const component = render(
+    const { baseElement } = render(
         <Quote data={str} index={0} length={1} isValid={true} />,
     )
 
-
-    expect(component).toMatchSnapshot()
+    expect(baseElement).toMatchSnapshot()
 })
 
 test('Quote is invalid', () => {
-    const component = render(
+    const { baseElement } = render(
         <Quote data={str} index={0} length={1} isValid={false} />,
     )
 
-
-    expect(component).toMatchSnapshot()
+    expect(baseElement).toMatchSnapshot()
 })
